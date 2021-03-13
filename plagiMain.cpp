bf6f4ddb00fa46dde46595cb8456c7e4f06978b6
Ashley Alex Jacob <alexjacob260@gmail.com>
2021-03-14 00:36:58 +0500

    removed unneccessary statements and redundant use of std:: and camelCase naming convention followed

--- plagiMain.cpp
+++ plagiMain.cpp
@@ -18,25 +18,25 @@ char const * stopwords_file = "stopwords.txt";
 int score_accuracy = 1;
 int number_of_tests = 3;
 
-float dot_product(std::vector<int> a, std::vector<int> b) {
+float dot_product(vector<int> a, vector<int> b) {
     float sum = 0 ;
     for(int i=0; i<a.size(); i++)
         sum += a[i] * b[i];
     return sum;
 }
 
-float sum(std::vector<int> v) {
+float sum(vector<int> v) {
     float sumv = 0;
     for (auto& n : v)
         sumv += n;
     return sumv;
 }
 
-float get_multiplier(std::string word) {
+float get_multiplier(string word) {
     return word.length() * word.length();
 }
 
-float cosine_score(std::vector<int> bvector, std::vector<int> tvector) {
+float cosine_score(vector<int> bvector, vector<int> tvector) {
     return dot_product(bvector, tvector) / 
             (   sqrt(dot_product(bvector, bvector)) * 
                 sqrt(dot_product(tvector, tvector)) );
@@ -49,38 +49,42 @@ bool extCheck (string const &fileName, string const &extension) {
     else 
         return false;
 }
-// special attention
-void cleanString(std::string& str) {    
-    size_t i = 0;
-    size_t len = str.length();
+// Removes Comma and Characters from the given string it helps to compare
+// Input String: My,Name;is,Ashley.txt
+// After Cleaning The String
+// Output String: MyNameisAshleytxt
+void stringCleansing(string &inpStr) {    
+    int i = 0;
+    int len = inpStr.length();
     while(i < len){
-        if (!isalnum(str[i]) && str[i] != ' '){
-            str.erase(i,1);
+        if (!isalnum(inpStr[i]) && inpStr[i] != ' '){
+            inpStr.erase(i,1);
             len--;
         }else
             i++;
     }
 }
 
-std::string getfile(std::string filepath) {
-    std::ifstream mFile(filepath);
-    std::string output;
-    std::string temp;
-
-    while (mFile >> temp){
-        output += std::string(" ") + temp;
+string getFileData(string filePath) {
+    // ifstream object to access file data
+    ifstream dFile(filePath);
+    string retString,temp;
+    // accessing file data
+    while (dFile >> temp){
+        retString += string(" ") + temp;
     }
-
-    cleanString(output);
-    return output;
+// uncomment to unleash the power
+// cout<<retString<<"\n";
+   stringCleansing(retString);
+    return retString;
 }
 
-std::map<std::string, int> get_frequency(std::vector<std::string> tokens) {
-    std::map<std::string, int> freqs;
+map<string, int> get_frequency(vector<string> tokens) {
+ map<string, int> freqs;
     for (auto const & x : tokens)
         ++freqs[x];
-    std::vector<std::string> unique_tokens;
-    std::vector<int> freq_token;
+    vector<string> unique_tokens;
+    vector<int> freq_token;
     for (auto const & p : freqs){
         unique_tokens.push_back(p.first);
         freq_token.push_back(p.second);
@@ -90,20 +94,20 @@ std::map<std::string, int> get_frequency(std::vector<std::string> tokens) {
 }
 // we can minus to check
 // tokenization
-std::vector<std::string> string_to_token(std::string str) {
-    std::istringstream mstream(str);
-    return std::vector<std::string>(std::istream_iterator<std::string>{mstream}, std::istream_iterator<std::string>{});
+vector<string> stringToToken(string inpString) {
+    istringstream dStream(inpString);
+    return vector<string>(istream_iterator<string>{dStream}, istream_iterator<string>{});
 }
 
-float ngram_score(std::vector<std::string> base, std::vector<std::string> target, int n) {
-    std::vector<std::vector<std::string>> bngrams;
-    std::vector<std::vector<std::string>> tngrams;
-    std::vector<std::string> temp;
+float ngram_score(vector<string> dataBase, vector<string> target, int n) {
+    vector<vector<string>> bngrams;
+    vector<vector<string>> tngrams;
+    vector<string> temp;
 
-    for(int i=0; i<=base.size()-n; i++) {
+    for(int i=0; i<=dataBase.size()-n; i++) {
         temp.clear();
         for(int j=i; j<i+n; j++) 
-            temp.push_back(base[j]);
+            temp.push_back(dataBase[j]);
         bngrams.push_back(temp);
     }
 
@@ -127,22 +131,22 @@ float ngram_score(std::vector<std::string> base, std::vector<std::string> target
     return 1.0 * shared / total;
 }
 
-float tokenize_test(std::vector<std::string> b_tokens, std::vector<std::string> t_tokens) {
-    std::ifstream infile(stopwords_file);
-    std::string stopword;
-    while (infile >> stopword){
-        t_tokens.erase(std::remove(t_tokens.begin(), t_tokens.end(), stopword), t_tokens.end());
+float tokenizeTest(vector<string> dbTokens, vector<string> tarTokens) {
+    ifstream inFile(stopwords_file);
+    string stopword;
+    while (inFile >> stopword){
+        tarTokens.erase(remove(tarTokens.begin(), tarTokens.end(), stopword), tarTokens.end());
     }
     
-    auto t_freqs = get_frequency(t_tokens);
-    auto b_freqs = get_frequency(b_tokens);
+    auto tarFreqs = get_frequency(tarTokens);
+    auto dbFreqs = get_frequency(dbTokens);
     
     int shared = 0;
     int total = 0;
     
-    for(auto const & word : t_freqs) {
-        auto search = b_freqs.find(word.first);
-        if(search != b_freqs.end()){
+    for(auto const & word : tarFreqs) {
+        auto search = dbFreqs .find(word.first);
+        if(search != dbFreqs .end()){
             shared += std::min(word.second, search->second) * get_multiplier(word.first);
             total += word.second * get_multiplier(word.first);
         } else {
@@ -154,64 +158,63 @@ float tokenize_test(std::vector<std::string> b_tokens, std::vector<std::string> 
     return score;
 }
 
-float ngram_test(std::vector<std::string> b_tokens, std::vector<std::string> t_tokens) {
-    std::vector<int> tests {3, 5, 7};
-    std::vector<int> weights {3, 5, 7};
+float ngramTest(vector<string> dbTokens,vector<string> tarTokens) {
+   vector<int> tests {3, 5, 7};
+   vector<int> weights {3, 5, 7};
+   vector<float> ngresults;
 
-    std::vector<float> ngresults;
-
-    ngresults.push_back(ngram_score(b_tokens, t_tokens, 3));
-    ngresults.push_back(ngram_score(b_tokens, t_tokens, 5));
-    ngresults.push_back(ngram_score(b_tokens, t_tokens, 7));
+    ngresults.push_back(ngram_score(dbTokens, tarTokens, 3));
+    ngresults.push_back(ngram_score(dbTokens, tarTokens, 5));
+    ngresults.push_back(ngram_score(dbTokens, tarTokens, 7));
 
     float score = 10 * pow((ngresults[0]*weights[0] + ngresults[1]*weights[1] + ngresults[2]*weights[2])/sum(weights), 0.4);
     return score;
 }
 
-float cosine_test(std::vector<std::string> b_tokens, std::vector<std::string> t_tokens) {
-    std::ifstream infile(stopwords_file);
-    std::string stopword;
+float cosineTest(vector<string> dbTokens,vector<string> tarTokens) {
+    ifstream infile(stopwords_file);
+    string stopword;
     while (infile >> stopword) {
-        t_tokens.erase(std::remove(t_tokens.begin(), t_tokens.end(), stopword), t_tokens.end());
-        b_tokens.erase(std::remove(b_tokens.begin(), b_tokens.end(), stopword), b_tokens.end());
+        tarTokens.erase(remove(tarTokens.begin(), tarTokens.end(), stopword), tarTokens.end());
+        dbTokens.erase(remove(dbTokens.begin(), dbTokens.end(), stopword), dbTokens.end());
     }
 
-    std::vector<std::string> all_tokens;
-    all_tokens.reserve( t_tokens.size() + b_tokens.size() );
-    all_tokens.insert( all_tokens.end(), t_tokens.begin(), t_tokens.end() );
-    all_tokens.insert( all_tokens.end(), b_tokens.begin(), b_tokens.end() );
+    vector<string> all_tokens;
+    all_tokens.reserve( tarTokens.size() + dbTokens.size() );
+    all_tokens.insert( all_tokens.end(), tarTokens.begin(), tarTokens.end() );
+    all_tokens.insert( all_tokens.end(), dbTokens.begin(), dbTokens.end() );
     sort( all_tokens.begin(), all_tokens.end() );
     all_tokens.erase( unique( all_tokens.begin(), all_tokens.end() ), all_tokens.end() );
 
-    auto t_freqs = get_frequency(t_tokens);
-    auto b_freqs = get_frequency(b_tokens);
+    auto tarFreqs = get_frequency(tarTokens);
+    auto dbFreqs  = get_frequency(dbTokens);
 
-    std::vector<int> b_vector;
-    std::vector<int> t_vector;
+    vector<int> dbVector;
+    vector<int> tarVector;
 
     for(auto & token: all_tokens) {
-        auto search = b_freqs.find(token);
-        if(search != b_freqs.end()) {
-            b_vector.push_back(search->second);
+        auto search = dbFreqs .find(token);
+        if(search != dbFreqs .end()) {
+            dbVector.push_back(search->second);
         } else {
-            b_vector.push_back(0);
+            dbVector.push_back(0);
         }
 
-        search = t_freqs.find(token);
-        if(search != t_freqs.end()) {
-            t_vector.push_back(search->second);
+        search = tarFreqs.find(token);
+        if(search != tarFreqs.end()) {
+            tarVector.push_back(search->second);
         } else {
-            t_vector.push_back(0);
+            tarVector.push_back(0);
         }
     }
 
-    float score = 10.0 * cosine_score(b_vector, t_vector);
+    float score = 10.0 * cosine_score(dbVector, tarVector);
 
     return score;
 }
 
-void get_verdict(std::vector<float> t, std::vector<std::string> m) {
-    std::vector<int> weights (t.size(), 0);
+void get_verdict(vector<float> testScores, vector<string> matchingRes) {
+    vector<int> weights (testScores.size(), 0);
     
     /**************************
         test1 - tokenize test
@@ -223,8 +226,8 @@ void get_verdict(std::vector<float> t, std::vector<std::string> m) {
     weights[1] = 4;
     weights[2] = 3;
 
-    float final_score = (t[0]*weights[0] + t[1]*weights[1] + t[2]*weights[2])/sum(weights);
-    std::string verdict;
+    float final_score = (testScores[0]*weights[0] + testScores[1]*weights[1] + testScores[2]*weights[2])/sum(weights);
+    string verdict;
 
     if(final_score < 1)
         verdict = "Not plagiarised";
@@ -235,22 +238,22 @@ void get_verdict(std::vector<float> t, std::vector<std::string> m) {
     else
         verdict = "Highly plagiarised";
 
-    m.erase( remove( m.begin(), m.end(), "" ), m.end() );
-    sort( m.begin(), m.end() );
-    m.erase( unique( m.begin(), m.end() ), m.end() );
+    matchingRes.erase( remove( matchingRes.begin(), matchingRes.end(), "" ), matchingRes.end() );
+    sort( matchingRes.begin(), matchingRes.end() );
+    matchingRes.erase( unique( matchingRes.begin(), matchingRes.end() ), matchingRes.end() );
 
-    std::cout<<"********************************************"<<std::endl;
-    std::cout<<"\tFinal score: "<<final_score<<std::endl;
-    std::cout<<"\tVerdict: "<<verdict<<std::endl;
+    cout<<"********************************************"<<std::endl;
+    cout<<"\tFinal score: "<<final_score<<endl;
+    cout<<"\tVerdict: "<<verdict<<endl;
     if(verdict != "Not plagiarised") {
-        std::cout<<"\tMatch found in:"<<std::endl;
-        if(m.size() == 0)
-            std::cout<<"\t-nil-"<<std::endl;
-        for (auto const & file : m)
-            std::cout<<"\t\t"<<file<<std::endl;
+        cout<<"\tMatch found in:"<<endl;
+        if(matchingRes.size() == 0)
+            cout<<"\t-nil-"<<endl;
+        for (auto const & file : matchingRes)
+           cout<<"\t\t"<<file<<endl;
     }
     
-    std::cout<<"********************************************"<<std::endl;
+        cout<<"********************************************"<<endl;
 
 }
 
@@ -258,11 +261,10 @@ int main(int argc, char *argv[]) {
     DIR *dir; // for target folder
     DIR *dirB;
     struct dirent *dir_object;
-    std::string target_file;
-    std::string base_file;
-
-    std::string target;
-    std::string base;
+    string target_file;
+    string base_file;
+    string target;
+    string base;
 
     float temp;
 
@@ -272,36 +274,35 @@ int main(int argc, char *argv[]) {
                 // target file name
                 printf ("\nPlagiarism scores for %s\n", dir_object->d_name);
                 // generate Path dynamically
-                target_file = target_folder + std::string("/") + dir_object->d_name;
-                target = getfile(target_file);
+                target_file = target_folder + string("/") + dir_object->d_name;
+                target = getFileData(target_file);
             // vector files declared variables
-                std::vector<float> test(number_of_tests, 0.0);
-                std::vector<std::string> match(number_of_tests, "");
+                vector<float> test(number_of_tests, 0.0);
+               vector<string> match(number_of_tests, "");
 // database directory
                 if ((dirB = opendir (database)) != NULL) {
                     while ((dir_object = readdir (dirB)) != NULL)
                         if(extCheck((string)dir_object->d_name, ".txt")){
                            // path generate
-                            base_file = database + std::string("/") + dir_object->d_name;
-                            
-                            base = getfile(base_file);
-// after getting ready for walima, now ready for bang
-
-                            auto b_tokens = string_to_token(base);
-                            auto t_tokens = string_to_token(target);
-//  Tokenization Done
+                            base_file = database + string("/") + dir_object->d_name;
+                        // accessing file data but by bit
+                            base = getFileData(base_file);
+                        // generating tokens from string
+                            auto databaseTokens = stringToToken(base);
+                            auto targetTokens = stringToToken(target);
+                //  Tokenization Done
                            // FiRST tEST RESULT
-                            temp = tokenize_test(b_tokens, t_tokens);
+                            temp = tokenizeTest(databaseTokens,targetTokens);
                             if(test[0] < temp) {
                                 test[0] = temp;
                                 match[0] = dir_object->d_name;
                             }
-                            temp = ngram_test(b_tokens, t_tokens);
+                            temp = ngramTest(databaseTokens,targetTokens);
                             if(test[1] < temp) {
                                 test[1] = temp;
                                 match[1] = dir_object->d_name;
                             }
-                            temp = cosine_test(b_tokens, t_tokens);
+                            temp = cosineTest(databaseTokens,targetTokens);
                             if(test[2] < temp) {
                                 test[2] = temp;
                                 match[2] = dir_object->d_name;
@@ -311,13 +312,13 @@ int main(int argc, char *argv[]) {
                     closedir (dirB);
                 }
 
-                std::cout<<"Test 1 score: "<<std::fixed<<std::setprecision(score_accuracy)<<test[0]<<"/10"<<std::endl;
-                std::cout<<"Test 2 score: "<<std::fixed<<std::setprecision(score_accuracy)<<test[1]<<"/10"<<std::endl;
-                std::cout<<"Test 3 score: "<<std::fixed<<std::setprecision(score_accuracy)<<test[2]<<"/10"<<std::endl;
+                cout<<"Test 1 score: "<<fixed<<setprecision(score_accuracy)<<test[0]<<"/10"<<endl;
+                cout<<"Test 2 score: "<<fixed<<setprecision(score_accuracy)<<test[1]<<"/10"<<endl;
+                cout<<"Test 3 score: "<<fixed<<setprecision(score_accuracy)<<test[2]<<"/10"<<endl;
 
                 get_verdict(test, match);
 
-                std::cout<<std::endl;
+              cout<<"\n";
             }
                 
         closedir (dir);
---
+++ test.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <algorithm>
+#include <iterator>
+#include <dirent.h>
+#include <map>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+// Removes Comma and Characters from the given string it helps to compare
+// Input String: My,Name;is,Ashley.txt
+// After Cleaning The String
+// Output String: MyNameisAshleytxt
+// tokenization
+vector<string> stringToToken(string inpString) {
+    istringstream dStream(inpString);
+    return vector<string>(istream_iterator<string>{dStream}, istream_iterator<string>{});
+}
+
+// void stringCleansing(string &inpStr) {    
+//     int i = 0;
+//     int len = inpStr.length();
+//     while(i < len){
+//         if (!isalnum(inpStr[i]) && inpStr[i] != ' '){
+//             inpStr.erase(i,1);
+//             len--;
+//         }else
+//             i++;
+//     }
+// }
+
+// string getFileData(string filePath) {
+//     // ifstream object to access file data
+//     ifstream dFile(filePath);
+//     string retString,temp;
+//     // accessing file data
+//     while (dFile >> temp){
+//         retString += string(" ") + temp;
+//     }
+// // uncomment to unleash the power
+// // cout<<retString<<"\n";
+//    stringCleansing(retString);
+//     return retString;
+// }
+
+
+int main(){
+
+    string test = "/home/ashley/work/spProject/SP Lab Project/target/ar.txt";
+	 //printf("%s\n",test);
+     cout<<test<<"\n";
+     auto rest = stringToToken(test);
+//     for(int i=rest.begin().;i!=rest.end();i++){
+
+   // cout<<"After Cleaning The String\n";
+    //cout<<test<<"\n";
+// string res =     getFileData(test);
+// cout<<res<<"\n";
+   	return 0;
+}
